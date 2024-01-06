405acbd04ee442a5647996e1ca45d798cd3b7f5d
Lâm Đức Kính <89448648+KinhLam@users.noreply.github.com>
2024-01-06 22:38:11 +0700

    Hoàn thành trang Login

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -6,8 +6,10 @@ import {
   StyleSheet,
   Image,
   ScrollView,
+  Modal,
+  ActivityIndicator,
 } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import IconBack from '../../components/IconBack';
 import IconVector from '../../components/IconVector';
 import CustomButton from '../../components/CustomButton';
@@ -15,10 +17,27 @@ import CustomButton from '../../components/CustomButton';
 const LoginScreen = () => {
   const navigation = useNavigation();
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [error, setError] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fixedPhoneNumber = '0987654321'; // Số điện thoại cố định
 
   const handleContinue = () => {
-    console.log('Số điện thoại: ', phoneNumber);
-    navigation.navigate('LoginNumberScreen');
+    setIsLoading(true);
+
+    // Simulate a 2-second delay before navigating to the next screen
+    setTimeout(() => {
+      setIsLoading(false);
+
+      if (phoneNumber === fixedPhoneNumber) {
+        console.log('Số điện thoại: ', phoneNumber);
+        navigation.navigate('LoginNumberScreen');
+      } else {
+        setError('Số điện thoại không đúng');
+        setIsError(true);
+      }
+    }, 2000);
   };
 
   const handleBackPress = () => {
@@ -41,13 +60,17 @@ const LoginScreen = () => {
           <Text style={styles.textHeader}>Xin chào!</Text>
         </View>
 
-        <View style={styles.inputContainer}>
+        <View style={[styles.inputContainer, isError && styles.errorInput]}>
           <TextInput
             placeholder="Nhập số điện thoại"
             style={styles.inputNumber}
             keyboardType="numeric"
             value={phoneNumber}
-            onChangeText={text => setPhoneNumber(text)}
+            onChangeText={(text) => {
+              setPhoneNumber(text);
+              setError('');
+              setIsError(true);
+            }}
           />
           <IconVector
             library="FontAwesome"
@@ -56,6 +79,7 @@ const LoginScreen = () => {
             color="#807A7A"
             style={styles.icon}
           />
+          <Text style={{ color: 'red' }}>{error}</Text>
         </View>
 
         <View>
@@ -63,7 +87,7 @@ const LoginScreen = () => {
             title="Đăng nhập"
             onPress={handleContinue}
             style={styles.button}
-            textStyle={[styles.textButton, {color: 'white'}]}
+            textStyle={[styles.textButton, { color: 'white' }]}
             uppercase={true}
           />
           <IconVector
@@ -75,10 +99,18 @@ const LoginScreen = () => {
           />
         </View>
       </View>
+
+      {/* Modal for Loading */}
+      <Modal transparent={true} animationType="slide" visible={isLoading}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <ActivityIndicator size="large" color="#23AC41" />
+          </View>
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -116,11 +148,14 @@ const styles = StyleSheet.create({
     position: 'relative',
     margin: 10,
   },
+  errorInput: {
+    borderColor: 'red', // Màu viền đỏ khi có lỗi
+  },
   icon: {
     position: 'absolute',
     top: '50%',
     left: 15,
-    transform: [{translateY: -5}],
+    transform: [{translateY: -13}],
   },
   iconButton: {
     position: 'absolute',
@@ -136,15 +171,30 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 20,
     marginBottom: 10,
+    fontSize: 15,
   },
   button: {
     height: 55,
     width: 271,
     padding: 10,
-    marginTop: 10,
+    marginTop: 5,
     marginLeft: '10%',
     borderRadius: 15,
     backgroundColor: '#23AC41',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(160, 151, 151, 0.33) 0.16)',
+  },
+  modalContent: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
